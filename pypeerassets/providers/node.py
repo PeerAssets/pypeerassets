02fad660afbb6b5ca1fc4f1c3a1fcf3c95f9fd0d
saeveritt <saeveritt@gmail.com>
2016-11-11 23:12:55 -0600

    Refactor unspent utxo data gathering

--- pypeerassets/providers/node.py
+++ pypeerassets/providers/node.py
@@ -14,24 +14,20 @@ def select_inputs(cls, total_amount):
     to never spend old transactions with a lot of coin age.
     Argument is intiger, returns list of apropriate UTXO's'''
 
-    my_addresses = [i["address"] for i in cls.listreceivedbyaddress()]
-
     utxo = []
     utxo_sum = float(-0.01) ## starts from negative due to minimal fee
     for tx in sorted(cls.listunspent(), key=itemgetter('confirmations')):
 
-        for v in cls.getrawtransaction(tx["txid"])["vout"]:
-            if v["scriptPubKey"]["addresses"][0] in my_addresses:
-                utxo.append({
-                    "txid": tx["txid"],
-                    "vout": v["n"],
-                    "ScriptSig": v["scriptPubKey"]["hex"]
-                })
-
-                utxo_sum += float(v["value"])
-                if utxo_sum >= total_amount:
-                    return utxo
-
+        utxo.append({
+            "txid": tx["txid"],
+            "vout": tx["vout"],
+            "ScriptSig": tx["scriptPubKey"]
+        })
+    
+        utxo_sum += float(tx["amount"])
+        if utxo_sum >= total_amount:
+            return utxo
+            
     if utxo_sum < total_amount:
         raise ValueError("Not enough funds.")
 
