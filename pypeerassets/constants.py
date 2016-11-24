fae569d940c36fd8b12105743ef40226fcd36b86
Peerchemist <dx.1178@gmail.com>
2016-11-24 12:29:15 +0100

    add constants file, to host P2TH tag keys.

---
+++ pypeerassets/constants.py
@@ -0,0 +1,18 @@
+
+'''various contants'''
+
+### P2TH tags for Peercoin mainnet/testnet ### ### ### 
+
+## mainnet
+mainnet_PAPROD = "U624wXL6iT7XZ9qeHsrtPGEiU78V1YxDfwq75Mymd61Ch56w47KE"
+mainnet_PAPROD_addr = "PAprodbYvZqf4vjhef49aThB9rSZRxXsM6"
+mainnet_PATEST = "UAbxMGQQKmfZCwKXAhUQg3MZNXcnSqG5Z6wAJMLNVUAcyJ5yYxLP"
+mainnet_PATEST_addr = "PAtesth4QreCwMzXJjYHBcCVKbC4wjbYKP"
+
+## testnet
+testnet_PAPROD = "cTJVuFKuupqVjaQCFLtsJfG8NyEyHZ3vjCdistzitsD2ZapvwYZH"
+testnet_PAPROD_addr = "miHhMLaMWubq4Wx6SdTEqZcUHEGp8RKMZt"
+testnet_PATEST = "cQToBYwzrB3yHr8h7PchBobM3zbrdGKj2LtXqg7NQLuxsHeKJtRL"
+testnet_PATEST_addr = "mvfR2sSxAfmDaGgPcmdsTwPqzS6R9nM5Bo"
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ###
