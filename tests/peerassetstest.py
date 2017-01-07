879cc1033f171229c7c72b840d26ea10591a39d0
Peerchemist <dx.1178@gmail.com>
2017-01-07 11:26:38 +0100

    tests for invalid deck spawn metainfo

--- tests/peerassetstest.py
+++ tests/peerassetstest.py
@@ -8,12 +8,22 @@ class PeerAssetsTestCase(unittest.TestCase):
         '''tests if loading of deck parameteres from protobuf'''
 
         string = b'\x08\x01\x12\x0cmy_test_deck\x18\x03 \x02'
-
         self.assertEqual(parse_deckspawn_metainfo(string), {'issue_mode': 2,
                                                             'name': 'my_test_deck',
                                                             'number_of_decimals': 3,
                                                             'version': 1})
-        #incomplete_string = b'\x08\x01\x12\x03ttt'
+
+    def test_parse_invalid_deckspawn_metainfo(self):
+        '''test if it fails on faulty deckspawn metainfo'''
+
+        string = b'\x12\x06faulty\x18\x01' # without version and issue mode
+        self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
+
+        string = b'\x08\x01\x18\x05 \x04' # without deck name
+        self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
+
+        string = b'\x08\x01\x12\x06faulty \x02' # without number_of_decimals
+        self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
 
 
 if __name__ == '__main__':
