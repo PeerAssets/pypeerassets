6cc434c86528517597ab49dd7eb79a4d4f28c662
Peerchemist <dx.1178@gmail.com>
2017-01-11 18:12:33 +0100

    Add find_all_valid_decks function to scan the blockchain for valid decks.

--- pypeerassets/pa.py
+++ pypeerassets/pa.py
@@ -3,15 +3,35 @@
 
 from pypeerassets import paproto, pautils, RpcNode
 
+def find_all_valid_decks(node, testnet=True, prod_or_test="prod"):
+    '''scan the blockchain for PeerAssets decks, returns list of deck objects.'''
+
+    decks = []
+    deck_spawns = pautils.find_deck_spawns(node) # find all deck_spawns on PAProd P2TH
+
+    for i in deck_spawns:
+        try:
+            pautils.validate_deckspawn_p2th(node, i, testnet=testnet, prod_or_test="prod")
+            d = pautils.parse_deckspawn_metainfo(pautils.read_tx_opreturn(node, i))
+            d["asset_id"] = i
+            decks.append(Deck(**d))
+
+        except AssertionError:
+            pass
+
+    return decks
+
 class Deck:
 
-    def __init__(self, version, name, number_of_decimals, issue_mode, asset_id=None):
+    def __init__(self, version, name, number_of_decimals, issue_mode, asset_specific_data, asset_id=None):
         '''initialize deck object, load from dictionary Deck(**dict) or initilize with kwargs Deck(1, "deck", 3, 2)'''
 
         self.version = version # protocol version
         self.name = name # deck name
         self.issue_mode = issue_mode # deck issue mode
-        self.asset_id = None
+        self.number_of_decimals = number_of_decimals
+        self.asset_specific_data = asset_specific_data # optional metadata for the deck
+        self.asset_id = asset_id
 
     @property
     def metainfo_to_protobuf(self):
