9954f8c489e23854c43b429097899f492b3b43df
Peerchemist <dx.1178@gmail.com>
2017-01-11 20:41:46 +0100

    init: reflect name change.

--- pypeerassets/__init__.py
+++ pypeerassets/__init__.py
@@ -1,4 +1,4 @@
 from pypeerassets.kutil import Kutil
-from pypeerassets.providers.rpc import RpcNode
+from pypeerassets.providers.rpcnode import RpcNode
 from pypeerassets.providers.mintr import Mintr
 from pypeerassets.providers import mock
