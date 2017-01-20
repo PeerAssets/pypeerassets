5749a997247cd7ed37fd8087468557a9ee193e72
Peerchemist <dx.1178@gmail.com>
2017-01-20 10:42:34 +0100

    pautils: small tweaks on find_deck_spawns

--- pypeerassets/pautils.py
+++ pypeerassets/pautils.py
@@ -46,10 +46,11 @@ def load_test_p2th_privkeys_into_node(node):
         except Exception:
             return {"error": "Loading P2TH privkey failed."}
 
-def find_deck_spawns(node, prod_or_test="prod"):
-    '''find deck spawn transactions via local node, it requiers that Deck spawn P2TH were imported in local node.'''
+def find_deck_spawns(node, prod=True):
+    '''find deck spawn transactions via local node,
+    it requires that Deck spawn P2TH were imported in local node.'''
 
-    if prod_or_test == "prod":
+    if prod:
         decks = [i["txid"] for i in node.listtransactions("PAPROD")]
     else:
         decks = [i["txid"] for i in node.listtransactions("PATEST")]
