65de6cd61d912a5bebf1bc016e15366d935b5851
Peerchemist <dx.1178@gmail.com>
2017-02-06 12:32:30 +0100

    tests: add P2TH generation test.

--- tests/peerassetstest.py
+++ tests/peerassetstest.py
@@ -1,5 +1,6 @@
 import unittest
-from pypeerassets.peerassets import parse_deckspawn_metainfo
+from pypeerassets.main import parse_deckspawn_metainfo
+from pypeerassets import Kutil
 
 class PeerAssetsTestCase(unittest.TestCase):
     '''tests for protobuf manipulation'''
@@ -30,6 +31,31 @@ class PeerAssetsTestCase(unittest.TestCase):
         string = b'\x08\x01\x12\x06faulty \x02' # without number_of_decimals
         self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
 
+
+class P2THTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('''Starting PeerAssets tests.
+                This class handles all things PeerAssets protocol.''')
+
+        cls.txid = "c23375caa1ba3b0eec3a49fff5e008dede0c2761bb31fddd830da32671c17f84"
+
+    def test_p2th_address_generation(self):
+        '''generating deck p2th from deck spawn id'''
+
+        key = Kutil(network="ppc", privkey=self.txid)
+
+        self.assertEqual(key.address, "PRoUKDUhA1vgBseJCaGMd9AYXdQcyEjxu9")
+        self.assertEqual(key.wif, "UBctiEkfxpU2HkyTbRKjiGHT5socJJwCny6ePfUtzo8Jad9wVzeA")
+
+    def test_testnet_p2th_address_generation(self):
+        '''generating testnet deck p2th from deck spawn id'''
+
+        key = Kutil(network="tppc", privkey=self.txid.encode())
+
+        self.assertEqual(key.address, "mxjFTJApv7sjz9T9a4vCnAQbmsqSoL8VWo")
+        self.assertEqual(key.wif, "cU6CjGw3mRmirjiUZfRkJ1aj2D493k7uuhywj6tCVbLAMABy4MwU")
 
 if __name__ == '__main__':
     unittest.main()
