4324835f8f7839ddd9c58025c70e319563c67bfc
Peerchemist <dx.1178@gmail.com>
2017-02-13 19:14:32 +0100

    add card_burn tx.

--- pypeerassets/main.py
+++ pypeerassets/main.py
@@ -282,3 +282,37 @@ def card_issue(deck, card_transfer, inputs, change_address, testnet=True, prod=T
 
     return transactions.make_raw_transaction(network, inputs['utxos'], outputs)
 
+def card_burn(deck, card_transfer, inputs, change_address, testnet=True, prod=True):
+    '''card burn transaction, cards are burned by sending the cards back to deck issuer'''
+
+    assert deck.issuer in card_transfer.receivers, {"error": "One of the recipients must be deck issuer."}
+
+    if testnet:
+        p2th_fee = constants.testnet_p2th_fee
+        network = "tppc"
+    else:
+        p2th_fee = constants.mainnet_p2th_fee
+        network = "ppc"
+
+    tx_fee = float(0.01) ## make it static for now, make proper logic later
+
+    for utxo in inputs['utxos']:
+        utxo['txid'] = unhexlify(utxo['txid'])
+        utxo['scriptSig'] = unhexlify(utxo['scriptSig'])
+
+    outputs = [
+        {"redeem": p2th_fee, "outputScript": transactions.monosig_script(deck.p2th_address)},
+        {"redeem": 0, "outputScript": transactions.op_return_script(card_transfer.metainfo_to_protobuf)}
+    ]
+
+    for addr in card_transfer.receivers:
+        outputs.append({"redeem": 0, "outputScript": transactions.monosig_script(addr)
+                       })
+
+    outputs.append(
+        {"redeem": float(inputs['total']) - float(tx_fee) - float(p2th_fee),
+         "outputScript": transactions.monosig_script(change_address)
+        })
+
+    return transactions.make_raw_transaction(network, inputs['utxos'], outputs)
+
