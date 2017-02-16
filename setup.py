e316fdad82d47fdce01dcd4e2a3c8e0d4ce86b82
Peerchemist <dx.1178@gmail.com>
2017-02-16 18:13:04 +0100

    add keywords to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='pypeerassets',
       version='0.1',
       description='Python implementation of the PeerAssets protocol.',
+      keywords=["blockchain", "digital asset", "protocol"],
       url='https://github.com/peerassets/pypeerassets',
       author='PeerAssets',
       author_email='peerchemist@protonmail.ch',
