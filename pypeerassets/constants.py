55b2200257418b71b586a89c9f625424eef8b6a3
Peerchemist <dx.1178@gmail.com>
2017-02-18 12:22:28 +0100

    refactor constants to allow dynamic parametere querying.

--- pypeerassets/constants.py
+++ pypeerassets/constants.py
@@ -1,24 +1,35 @@
+'''various protocol contants'''
 
-'''various contants'''
+from collections import namedtuple
 
-### P2TH tags for Peercoin mainnet/testnet ### ### ### 
+Network = namedtuple('Network', [
+    'network_name',
+    'network_shortname',
+    'P2TH_wif',
+    'P2TH_addr',
+    'test_P2TH_wif',
+    'test_P2TH_addr',
+    'P2TH_fee',
+])
 
-## mainnet
-mainnet_PAPROD = "U624wXL6iT7XZ9qeHsrtPGEiU78V1YxDfwq75Mymd61Ch56w47KE"
-mainnet_PAPROD_addr = "PAprodbYvZqf4vjhef49aThB9rSZRxXsM6"
-mainnet_PATEST = "UAbxMGQQKmfZCwKXAhUQg3MZNXcnSqG5Z6wAJMLNVUAcyJ5yYxLP"
-mainnet_PATEST_addr = "PAtesth4QreCwMzXJjYHBcCVKbC4wjbYKP"
+params = (
 
-## testnet
-testnet_PAPROD = "cTJVuFKuupqVjaQCFLtsJfG8NyEyHZ3vjCdistzitsD2ZapvwYZH"
-testnet_PAPROD_addr = "miHhMLaMWubq4Wx6SdTEqZcUHEGp8RKMZt"
-testnet_PATEST = "cQToBYwzrB3yHr8h7PchBobM3zbrdGKj2LtXqg7NQLuxsHeKJtRL"
-testnet_PATEST_addr = "mvfR2sSxAfmDaGgPcmdsTwPqzS6R9nM5Bo"
+    ## PPC mainnet
+    Network("Peercoin", "ppc", "U624wXL6iT7XZ9qeHsrtPGEiU78V1YxDfwq75Mymd61Ch56w47KE",
+            "PAprodbYvZqf4vjhef49aThB9rSZRxXsM6", "UAbxMGQQKmfZCwKXAhUQg3MZNXcnSqG5Z6wAJMLNVUAcyJ5yYxLP",
+            "PAtesth4QreCwMzXJjYHBcCVKbC4wjbYKP", float(0.01)),
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ###
+    ## PPC testnet
+    Network("Peercoin-testnet", "tppc", "cTJVuFKuupqVjaQCFLtsJfG8NyEyHZ3vjCdistzitsD2ZapvwYZH",
+            "miHhMLaMWubq4Wx6SdTEqZcUHEGp8RKMZt", "cQToBYwzrB3yHr8h7PchBobM3zbrdGKj2LtXqg7NQLuxsHeKJtRL",
+            "mvfR2sSxAfmDaGgPcmdsTwPqzS6R9nM5Bo", float(0.01))
+)
 
-### P2TH fee
+def param_query(query):
+    '''find matching parameter among the networks'''
 
-mainnet_p2th_fee = float(0.01)
-testnet_p2th_fee = float(0.01)
+    for network in params:
+        for field in network:
+            if field == query:
+                return network
 
