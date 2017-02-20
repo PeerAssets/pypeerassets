8ee49fa19db468a42c83c516c50e44c94fb4312b
Peerchemist <dx.1178@gmail.com>
2017-02-20 10:48:57 +0100

    examples: testnet argument is no longer needed for the deck_spawn

--- examples/deck_spawn.py
+++ examples/deck_spawn.py
@@ -12,7 +12,7 @@ change_addr = "mwkFUPUrh6LsXyMvBY2mz6btiJjuTxGgT8"
 
 new_deck = pa.Deck("my_new_testnet_deck", 2, "MULTI", asset_specific_data="hello world.")
 
-raw_tx = hexlify(pa.deck_spawn(new_deck, "tppc", utxo, change_addr)).decode()
+raw_tx = hexlify(pa.deck_spawn(new_deck, utxo, change_addr)).decode()
 
 signed = provider.signrawtransaction(raw_tx)
 
