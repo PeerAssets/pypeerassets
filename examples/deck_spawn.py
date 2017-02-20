1621eee28b8a629e0eeaf78d09357482b568e6cf
Peerchemist <dx.1178@gmail.com>
2017-02-20 11:06:01 +0100

    improve examples: deck_spawn

--- examples/deck_spawn.py
+++ examples/deck_spawn.py
@@ -3,14 +3,14 @@ Spawn new PeerAssets deck on Peercoin testnet using local testnet node.
 '''
 
 import pypeerassets as pa
-fron binascii import hexlify
+from binascii import hexlify
 
 provider = pa.RpcNode(testnet=True)
 
 utxo = provider.select_inputs(0.02) ## we need 0.02 PPC
 change_addr = "mwkFUPUrh6LsXyMvBY2mz6btiJjuTxGgT8" 
 
-new_deck = pa.Deck("my_new_testnet_deck", 2, "MULTI", asset_specific_data="hello world.")
+new_deck = pa.Deck("my_new_testnet_deck", number_of_decimals=2, issue_mode="MULTI", asset_specific_data="hello world.", network="tppc")
 
 raw_tx = hexlify(pa.deck_spawn(new_deck, utxo, change_addr)).decode()
 
