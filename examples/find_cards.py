abaec362b6bff82e6b190f32e64b4294aed25f0c
Peerchemist <dx.1178@gmail.com>
2017-02-23 20:14:40 +0100

    add find_card example.

---
+++ examples/find_cards.py
@@ -0,0 +1,27 @@
+'''
+Find card transfers for <deck>.
+'''
+
+import sys
+import pypeerassets as pa
+provider = pa.RpcNode(testnet=True)
+
+print("Searching for deck {deck}.".format(deck=sys.argv[1]))
+deck = pa.find_deck(provider, sys.argv[1])[0]
+cards = pa.find_card_transfers(provider, deck)
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("This deck issuer has issued: {cards} cards.".format(
+      cards=sum([i.amounts[0] for i in cards if i.type == "CardIssue"])))
+
+print("Peers on this deck have transacted {cards} cards in {transfers} card transactions.".format(
+      cards=sum([i.amounts[0] for i in cards if i.type == "CardTransfer"]),
+      transfers=len([i for i in cards if i.type == "CardTransfer"])
+      ))
+
+print("Peers on this deck have burned {cards} cards in {transfers} card transactions.".format(
+      cards=sum([i.amounts[0] for i in cards if i.type == "CardBurn"]),
+      transfers=len([i for i in cards if i.type == "CardBurn"])
+     ))
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
