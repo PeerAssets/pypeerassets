cdf1f647098ce3bceb162dc64610ae331a272452
Peerchemist <dx.1178@gmail.com>
2017-02-24 17:38:08 +0100

    amounts -> amount

--- examples/find_cards.py
+++ examples/find_cards.py
@@ -11,16 +11,18 @@ deck = pa.find_deck(provider, sys.argv[1])[0]
 cards = pa.find_card_transfers(provider, deck)
 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print("This deck issuer has issued: {cards} cards.".format(
-      cards=sum([i.amounts[0] for i in cards if i.type == "CardIssue"])))
+print("This deck issuer has issued: {cards} cards in {transfers} card issue transactions.".format(
+      cards=sum([i.amount[0] for i in cards if i.type == "CardIssue"]),
+      transfers=len([i for i in cards if i.type == "CardIssue"])
+      ))
 
 print("Peers on this deck have transacted {cards} cards in {transfers} card transactions.".format(
-      cards=sum([i.amounts[0] for i in cards if i.type == "CardTransfer"]),
+      cards=sum([i.amount[0] for i in cards if i.type == "CardTransfer"]),
       transfers=len([i for i in cards if i.type == "CardTransfer"])
       ))
 
 print("Peers on this deck have burned {cards} cards in {transfers} card transactions.".format(
-      cards=sum([i.amounts[0] for i in cards if i.type == "CardBurn"]),
+      cards=sum([i.amount[0] for i in cards if i.type == "CardBurn"]),
       transfers=len([i for i in cards if i.type == "CardBurn"])
      ))
 
