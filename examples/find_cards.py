fc1b68bf601d397f1ca3ca7a06ba1a15bd5779b3
Peerchemist <dx.1178@gmail.com>
2017-02-26 15:03:18 +0100

    find_cards: round to deck.number_of_decimals

--- examples/find_cards.py
+++ examples/find_cards.py
@@ -12,17 +12,17 @@ cards = pa.find_card_transfers(provider, deck)
 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("This deck issuer has issued: {cards} cards in {transfers} card issue transactions.".format(
-      cards=sum((i.amount[0] for i in cards if i.type == "CardIssue")),
+      cards=round(sum((i.amount[0] for i in cards if i.type == "CardIssue")), deck.number_of_decimals),
       transfers=len([i for i in cards if i.type == "CardIssue"])
       ))
 
 print("Peers on this deck have transacted {cards} cards in {transfers} card transactions.".format(
-      cards=sum((i.amount[0] for i in cards if i.type == "CardTransfer")),
+      cards=round(sum((i.amount[0] for i in cards if i.type == "CardTransfer")), deck.number_of_decimals),
       transfers=len([i for i in cards if i.type == "CardTransfer"])
       ))
 
 print("Peers on this deck have burned {cards} cards in {transfers} card transactions.".format(
-      cards=sum((i.amount[0] for i in cards if i.type == "CardBurn")),
+      cards=round(sum((i.amount[0] for i in cards if i.type == "CardBurn")), deck.number_of_decimals),
       transfers=len([i for i in cards if i.type == "CardBurn"])
      ))
 
