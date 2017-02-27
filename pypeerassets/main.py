c021b1a9305aff6b03874c27b68fb3f09f6834f2
Peerchemist <dx.1178@gmail.com>
2017-02-27 19:00:31 +0100

    move Deck and CardTranfer to protocol.py

--- pypeerassets/main.py
+++ pypeerassets/main.py
@@ -1,13 +1,12 @@
 
 '''contains main protocol logic like assembly of proof-of-timeline and parsing deck info'''
 
-import warnings
 import concurrent.futures
-from binascii import hexlify, unhexlify
-from pypeerassets import paproto, Kutil
-from pypeerassets.pautils import *
-from pypeerassets import transactions
-from .constants import param_query, params 
+from binascii import unhexlify
+from .protocol import *
+from .pautils import *
+from . import transactions
+from .constants import param_query, params
 from .networks import query, networks
 
 def find_all_valid_decks(provider, prod=True) -> list:
@@ -49,73 +48,6 @@ def find_deck(provider, key: str, prod=True) -> list:
     decks = find_all_valid_decks(provider, prod=prod)
     return [d for d in decks if key in d.__dict__.values()]
 
-class Deck:
-
-    def __init__(self, name: str, number_of_decimals: int, issue_mode: str, network: str, production: bool, version=1,
-                 asset_specific_data="", issuer="", time=None, asset_id=None):
-        '''
-        Initialize deck object, load from dictionary Deck(**dict) or initilize with kwargs Deck("deck", 3, "ONCE")
-        '''
-
-        self.version = version # protocol version
-        self.name = name # deck name
-        self.issue_mode = issue_mode # deck issue mode
-        assert isinstance(number_of_decimals, int), {"error": "number_of_decimals must be an integer"}
-        self.number_of_decimals = number_of_decimals
-        self.asset_specific_data = asset_specific_data # optional metadata for the deck
-        self.asset_id = asset_id
-        self.issuer = issuer
-        self.issue_time = time
-        self.network = network
-        self.production = production
-        if self.network.startswith("t"):
-            self.testnet = True
-        else:
-            self.testnet = False
-
-    @property
-    def p2th_address(self):
-        '''P2TH address of this deck'''
-
-        return Kutil(network=self.network, privkey=self.asset_id).address
-
-    @property
-    def p2th_wif(self):
-        '''P2TH privkey in WIF format'''
-
-        return Kutil(network=self.network, privkey=self.asset_id).wif
-
-    @property
-    def metainfo_to_protobuf(self):
-        '''encode deck into protobuf'''
-
-        deck = paproto.DeckSpawn()
-        deck.version = self.version
-        deck.name = self.name
-        deck.number_of_decimals = self.number_of_decimals
-        deck.issue_mode = deck.MODE.Value(self.issue_mode)
-        if not isinstance(self.asset_specific_data, bytes):
-            deck.asset_specific_data = self.asset_specific_data.encode()
-        else:
-            deck.asset_specific_data = self.asset_specific_data
-
-        proto = deck.SerializeToString()
-
-        if len(proto) > 80:
-            warnings.warn('\nMetainfo size exceeds maximum of 80bytes that fit into OP_RETURN.')
-
-        return proto
-
-    @property
-    def metainfo_to_dict(self):
-        '''encode deck into dictionary'''
-
-        return {
-            "version": self.version,
-            "name": self.name,
-            "number_of_decimals": self.number_of_decimals,
-            "issue_mode": self.issue_mode
-        }
 
 def deck_spawn(deck: Deck, inputs: list, change_address: str) -> bytes:
     '''Creates Deck spawn raw transaction.'''
@@ -214,88 +146,6 @@ def find_card_transfers(provider, deck: Deck) -> list:
                 cards.extend(result)
 
     return cards
-
-
-class CardTransfer:
-
-    def __init__(self, deck: Deck, receiver=[], amount=[], version=1, txid=None, sender=None, blockhash=0,
-                 blockseq=None, timestamp=None, asset_specific_data="", number_of_decimals=None):
-        '''CardTransfer object, used when parsing card_transfers from the blockchain
-        or when sending out new card_transfer.
-        It can be initialized by passing the **kwargs and it will do the parsing,
-        or it can be initialized with passed arguments.
-
-        * deck - instance of Deck object
-        * receivers - list of receivers
-        * amounts - list of amounts to be sent, must be float
-        * version - protocol version, default 1
-        * txid - transaction ID of CardTransfer
-        * sender - transaction sender
-        * blockhash - block ID where the tx was first included
-        * blockseq - order in which tx was serialized into block
-        * timestamp - unix timestamp of the block where it was first included
-        * asset_specific_data - extra metadata
-        * number_of_decimals - number of decimals for amount, inherited from Deck object'''
-
-        assert len(amount) == len(receiver), {"error": "Amount must match receiver."}
-        self.version = version
-        self.deck_id = deck.asset_id
-        self.txid = txid
-        self.sender = sender
-        self.timestamp = timestamp
-        self.asset_specific_data = asset_specific_data
-        self.p2th_address = deck.p2th_address
-        if not number_of_decimals:
-            self.number_of_decimals = deck.number_of_decimals
-        else:
-            self.number_of_decimals = number_of_decimals
-
-        self.receiver = receiver
-        assert len(self.receiver) < 20, {"error": "Too many receivers."}
-        self.amount = []
-
-        for i in amount:
-            if not isinstance(i, float): # if not float, than it needs to be converted to float
-                self.amount.append(exponent_to_amount(i, self.number_of_decimals))
-            else:
-                assert str(i)[::-1].find('.') <= self.number_of_decimals, {"error": "Too many decimals."}
-                self.amount.append(i)
-
-        if blockhash:
-            self.blockhash = blockhash
-            self.blockseq = blockseq
-        else:
-            self.blockhash = 0
-            self.blockseq = 0
-
-        if self.sender == deck.issuer:
-            self.type = "CardIssue"
-        elif self.receiver[0] == deck.issuer:
-            self.type = "CardBurn"
-        else:
-            self.type = "CardTransfer"
-
-    @property
-    def metainfo_to_protobuf(self):
-        '''encode card_transfer info to protobuf'''
-
-        card = paproto.CardTransfer()
-        card.version = self.version
-        card.amount.extend(
-            [amount_to_exponent(i, self.number_of_decimals) for i in self.amount]
-            )
-        card.number_of_decimals = self.number_of_decimals
-        if not isinstance(self.asset_specific_data, bytes):
-            card.asset_specific_data = self.asset_specific_data.encode()
-        else:
-            card.asset_specific_data = self.asset_specific_data
-
-        proto = card.SerializeToString()
-
-        if len(proto) > 80:
-            warnings.warn('\nMetainfo size exceeds maximum of 80bytes that fit into OP_RETURN.')
-
-        return proto
 
 def card_issue(deck: Deck, card_transfer: CardTransfer, inputs: list, change_address: str) -> bytes:
     '''Create card issue transaction.
---
+++ pypeerassets/protocol.py
@@ -0,0 +1,159 @@
+"""all things PeerAssets protocol."""
+
+__all__ = ['Deck', 'CardTransfer']
+
+import warnings
+from .kutil import Kutil
+from . import paproto
+from .pautils import amount_to_exponent, exponent_to_amount
+
+class Deck:
+
+    def __init__(self, name: str, number_of_decimals: int, issue_mode: str, network: str, production: bool, version=1,
+                 asset_specific_data="", issuer="", time=None, asset_id=None):
+        '''
+        Initialize deck object, load from dictionary Deck(**dict) or initilize with kwargs Deck("deck", 3, "ONCE")
+        '''
+
+        self.version = version # protocol version
+        self.name = name # deck name
+        self.issue_mode = issue_mode # deck issue mode
+        assert isinstance(number_of_decimals, int), {"error": "number_of_decimals must be an integer"}
+        self.number_of_decimals = number_of_decimals
+        self.asset_specific_data = asset_specific_data # optional metadata for the deck
+        self.asset_id = asset_id
+        self.issuer = issuer
+        self.issue_time = time
+        self.network = network
+        self.production = production
+        if self.network.startswith("t"):
+            self.testnet = True
+        else:
+            self.testnet = False
+
+    @property
+    def p2th_address(self):
+        '''P2TH address of this deck'''
+
+        return Kutil(network=self.network, privkey=self.asset_id).address
+
+    @property
+    def p2th_wif(self):
+        '''P2TH privkey in WIF format'''
+
+        return Kutil(network=self.network, privkey=self.asset_id).wif
+
+    @property
+    def metainfo_to_protobuf(self):
+        '''encode deck into protobuf'''
+
+        deck = paproto.DeckSpawn()
+        deck.version = self.version
+        deck.name = self.name
+        deck.number_of_decimals = self.number_of_decimals
+        deck.issue_mode = deck.MODE.Value(self.issue_mode)
+        if not isinstance(self.asset_specific_data, bytes):
+            deck.asset_specific_data = self.asset_specific_data.encode()
+        else:
+            deck.asset_specific_data = self.asset_specific_data
+
+        proto = deck.SerializeToString()
+
+        if len(proto) > 80:
+            warnings.warn('\nMetainfo size exceeds maximum of 80bytes that fit into OP_RETURN.')
+
+        return proto
+
+    @property
+    def metainfo_to_dict(self):
+        '''encode deck into dictionary'''
+
+        return {
+            "version": self.version,
+            "name": self.name,
+            "number_of_decimals": self.number_of_decimals,
+            "issue_mode": self.issue_mode
+        }
+
+
+class CardTransfer:
+
+    def __init__(self, deck: Deck, receiver=[], amount=[], version=1, txid=None, sender=None, blockhash=0,
+                 blockseq=None, timestamp=None, asset_specific_data="", number_of_decimals=None):
+        '''CardTransfer object, used when parsing card_transfers from the blockchain
+        or when sending out new card_transfer.
+        It can be initialized by passing the **kwargs and it will do the parsing,
+        or it can be initialized with passed arguments.
+
+        * deck - instance of Deck object
+        * receivers - list of receivers
+        * amounts - list of amounts to be sent, must be float
+        * version - protocol version, default 1
+        * txid - transaction ID of CardTransfer
+        * sender - transaction sender
+        * blockhash - block ID where the tx was first included
+        * blockseq - order in which tx was serialized into block
+        * timestamp - unix timestamp of the block where it was first included
+        * asset_specific_data - extra metadata
+        * number_of_decimals - number of decimals for amount, inherited from Deck object'''
+
+        assert len(amount) == len(receiver), {"error": "Amount must match receiver."}
+        self.version = version
+        self.deck_id = deck.asset_id
+        self.txid = txid
+        self.sender = sender
+        self.timestamp = timestamp
+        self.asset_specific_data = asset_specific_data
+        self.p2th_address = deck.p2th_address
+        if not number_of_decimals:
+            self.number_of_decimals = deck.number_of_decimals
+        else:
+            self.number_of_decimals = number_of_decimals
+
+        self.receiver = receiver
+        assert len(self.receiver) < 20, {"error": "Too many receivers."}
+        self.amount = []
+
+        for i in amount:
+            if not isinstance(i, float): # if not float, than it needs to be converted to float
+                self.amount.append(exponent_to_amount(i, self.number_of_decimals))
+            else:
+                assert str(i)[::-1].find('.') <= self.number_of_decimals, {"error": "Too many decimals."}
+                self.amount.append(i)
+
+        if blockhash:
+            self.blockhash = blockhash
+            self.blockseq = blockseq
+        else:
+            self.blockhash = 0
+            self.blockseq = 0
+
+        if self.sender == deck.issuer:
+            self.type = "CardIssue"
+        elif self.receiver[0] == deck.issuer:
+            self.type = "CardBurn"
+        else:
+            self.type = "CardTransfer"
+
+    @property
+    def metainfo_to_protobuf(self):
+        '''encode card_transfer info to protobuf'''
+
+        card = paproto.CardTransfer()
+        card.version = self.version
+        card.amount.extend(
+            [amount_to_exponent(i, self.number_of_decimals) for i in self.amount]
+            )
+        card.number_of_decimals = self.number_of_decimals
+        if not isinstance(self.asset_specific_data, bytes):
+            card.asset_specific_data = self.asset_specific_data.encode()
+        else:
+            card.asset_specific_data = self.asset_specific_data
+
+        proto = card.SerializeToString()
+
+        if len(proto) > 80:
+            warnings.warn('\nMetainfo size exceeds maximum of 80bytes that fit into OP_RETURN.')
+
+        return proto
+
