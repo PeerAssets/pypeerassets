6d750475b923a53880679cb677d0e58169e974c6
Peerchemist <dx.1178@gmail.com>
2017-04-09 23:12:40 +0200

    update pavoteproto.

--- pypeerassets/pavoteproto.py
+++ pypeerassets/pavoteproto.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='0005-on-chain-voting-transaction-specification.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n40005-on-chain-voting-transaction-specification.proto\"\xa6\x01\n\x04Vote\x12\x0f\n\x07version\x18\x01 \x01(\r\x12\x12\n\ncount_mode\x18\x02 \x01(\r\x12\x0f\n\x07\x63hoices\x18\x03 \x03(\t\x12\x1b\n\x13\x61sset_specific_data\x18\x04 \x01(\x0c\"K\n\x04MODE\x12\x08\n\x04NONE\x10\x00\x12\n\n\x06SIMPLE\x10\x01\x12\x17\n\x13WEIGHT_CARD_BALANCE\x10\x03\x12\x14\n\x10WEIGHT_CARD_DAYS\x10\x07\x62\x06proto3')
+  serialized_pb=_b('\n40005-on-chain-voting-transaction-specification.proto\"\xb5\x01\n\x04Vote\x12\x0f\n\x07version\x18\x01 \x01(\r\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\x12\x12\n\ncount_mode\x18\x03 \x01(\r\x12\x0f\n\x07\x63hoices\x18\x04 \x03(\t\x12\x15\n\rvote_metainfo\x18\x05 \x01(\x0c\"K\n\x04MODE\x12\x08\n\x04NONE\x10\x00\x12\n\n\x06SIMPLE\x10\x01\x12\x17\n\x13WEIGHT_CARD_BALANCE\x10\x03\x12\x14\n\x10WEIGHT_CARD_DAYS\x10\x07\x62\x06proto3')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -50,8 +50,8 @@ _VOTE_MODE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=148,
-  serialized_end=223,
+  serialized_start=163,
+  serialized_end=238,
 )
 _sym_db.RegisterEnumDescriptor(_VOTE_MODE)
 
@@ -71,22 +71,29 @@ _VOTE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='count_mode', full_name='Vote.count_mode', index=1,
-      number=2, type=13, cpp_type=3, label=1,
+      name='description', full_name='Vote.description', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='count_mode', full_name='Vote.count_mode', index=2,
+      number=3, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='choices', full_name='Vote.choices', index=2,
-      number=3, type=9, cpp_type=9, label=3,
+      name='choices', full_name='Vote.choices', index=3,
+      number=4, type=9, cpp_type=9, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='asset_specific_data', full_name='Vote.asset_specific_data', index=3,
-      number=4, type=12, cpp_type=9, label=1,
+      name='vote_metainfo', full_name='Vote.vote_metainfo', index=4,
+      number=5, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value=_b(""),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -105,7 +112,7 @@ _VOTE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=57,
-  serialized_end=223,
+  serialized_end=238,
 )
 
 _VOTE_MODE.containing_type = _VOTE
