fb627d03af8e772d3d341acf474b836c798dadc7
saeveritt <saeveritt@gmail.com>
2017-07-11 23:16:38 +0000

    Add crypto Submodule

---
+++ pypeerassets/crypto/fernet.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet as F
+from base64 import b64encode
+
+class Fernet:
+    def __init__(self, message, key):
+
+        if isinstance(key, str):
+            key = key.encode()
+        if isinstance(message, str):
+            message = message.encode()
+        
+        self.key = key
+        self.cipher = F(self.key)
+        self.message = message
+
+    def encrypt(self):
+        self.message =  self.cipher.encrypt(self.message)
+        return self.message
+    def decrypt(self):
+        self.message = self.cipher.decrypt(self.message)
+        return self.message
