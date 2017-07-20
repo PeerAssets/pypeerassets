56ff58ac22f9592117410a36cbf0b098b4cd13df
Peerchemist <dx.1178@gmail.com>
2017-07-20 10:10:00 +0200

    rewrite and expand transactions.py tests

--- test/transactionstest.py
+++ test/transactionstest.py
@@ -1,47 +1,113 @@
-import unittest
+import pytest
 from binascii import unhexlify, hexlify
 from pypeerassets.transactions import *
 
-class TransactionsTestCase(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        print('''Starting transactions, verify if transaction assembly/dissasembly is working as it should.''')
+def test_tx_buffer():
+    '''test tx_buffer transaction unpacking class'''
 
-    def test_get_hash_160(self):
+    rawtx = '''01000000c2376d59010000000000000000000000000000000000000000000000000000000000000000ffffffff0e033b5d04026102062f503253482fffffffff0100000000000000000000000000'''
 
-        address = "PUsvrXxav7ryMawmxZk9ShmGiuCJWjdPmo"
-        self.assertEqual(get_hash160(address), b'\xde\x8d\xa6A3P\x18\x8b\x95H\x8d\xb2\xf9\tmu\xb6U\xfc\xc7')
+    t = Tx_buffer(unhexlify(rawtx))
 
-    def test_transaction_dissasembly_test(self):
-        '''verifies that transaction dissasembly is in order'''
+    d = {
+        'data': b'\x01\x00\x00\x00\xc27mY\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\x0e\x03;]\x04\x02a\x02\x06/P2SH/\xff\xff\xff\xff\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+        'len': 78,
+        'ptr': 0}
 
-        tx = "10060b0349d3c84a7d88bd396a703d6df39c587bf8169fef73db46e7b346efe5"
-        ## getrawtx by ppcoind
-        rawtx = "0100000055842d5801893e129cc07ed2799eef39c3961baec2ce58e273ce51ae9f16fd92be9924bbf5020000004a493046022100b4de95eb9652b86fe3070ab9c1ccf60ec700638848589416f48bfaa0342c2d46022100ed7ff3ec83324c146e3ce8123854a62a9953f6ebd027b77330a1c2b9f0a5847501ffffffff020000000000000000007096050d000000002321027f2101a1eb0ef4f53ab48919a0f390dfcaddd6b8104324b4ef8d2654b08b3722ac00000000"
+    assert t.__dict__ == d
 
-        ## decoded by ppcoind
-        decoded_raw_tx = {
-            'vout': [
-                {'scriptPubKey': '', 'value': 0.0},
-                {'scriptPubKey': '21027f2101a1eb0ef4f53ab48919a0f390dfcaddd6b8104324b4ef8d2654b08b3722ac',
-                 'value': 218.47}
-            ],
-            'timestamp': 1479378005,
-            'version': 1,
-            'vin': [
-                {
-                    'vout': 2,
-                    'sequence': 4294967295,
-                    'scriptSig': '493046022100b4de95eb9652b86fe3070ab9c1ccf60ec700638848589416f48bfaa0342c2d46022100ed7ff3ec83324c146e3ce8123854a62a9953f6ebd027b77330a1c2b9f0a5847501',
-                    'txid': 'f5bb2499be92fd169fae51ce73e258cec2ae1b96c339ef9e79d27ec09c123e89'
-                }
-            ],
-            'locktime': 0
-        }
 
-        self.assertEqual(unpack_raw_transaction(unhexlify(rawtx)), decoded_raw_tx)
+def test_get_hash_160():
 
+    address = "PUsvrXxav7ryMawmxZk9ShmGiuCJWjdPmo"
+    assert get_hash160(address) == b'\xde\x8d\xa6A3P\x18\x8b\x95H\x8d\xb2\xf9\tmu\xb6U\xfc\xc7'
+
+
+@pytest.mark.parametrize("num", [0, 1, 5, 111])
+def test_op_push(num):
+
+    if num == 0:
+        assert op_push(num) == b'\x00'
+
+    if num == 1:
+        assert op_push(num) == b'\x01'
+
+    if num == 5:
+        assert op_push(num) == b'\x05'
+
+    if num == 111:
+        assert op_push(num) == b'Lo'
+
+
+@pytest.mark.parametrize("num", [0, 1, 5, 111])
+def test_var_int(num):
+
+    if num == 0:
+        assert var_int(num) == b'\x00'
+
+    if num == 1:
+        assert var_int(num) == b'\x01'
+
+    if num == 5:
+        assert var_int(num) == b'\x05'
+
+    if num == 111:
+        assert var_int(num) == b'o'
+
+
+@pytest.mark.parametrize("num", [0, 10, 32])
+def test_pack_uint64(num):
+
+    if num == 0:
+        assert pack_uint64(num) == b'\x00\x00\x00\x00\x00\x00\x00\x00'
+
+    if num == 10:
+        assert pack_uint64(num) == b'\n\x00\x00\x00\x00\x00\x00\x00'
+
+    if num == 32:
+        assert pack_uint64(num) == b' \x00\x00\x00\x00\x00\x00\x00'
+
+
+def test_monosig_script():
+
+    assert monosig_script("n2uLuStAtNYeJyzJCCJpTAueVAoAjX8erb") == b'v\xa9\x14\xea\x96\xaa\xab\xcb\xeb\x82f\xbe\xbb\xddd\xd1\xbcy(\xbe\x1b\xc0J\x88\xac'
+
+
+def test_op_return_script():
+
+    assert op_return_script("kokolomoj".encode()) == b'j\tkokolomoj'
+
+
+def test_transaction_dissasembly_test():
+    '''verifies that transaction dissasembly is in order'''
+
+    tx = "10060b0349d3c84a7d88bd396a703d6df39c587bf8169fef73db46e7b346efe5"
+    rawtx = "0100000055842d5801893e129cc07ed2799eef39c3961baec2ce58e273ce51ae9f16fd92be9924bbf5020000004a493046022100b4de95eb9652b86fe3070ab9c1ccf60ec700638848589416f48bfaa0342c2d46022100ed7ff3ec83324c146e3ce8123854a62a9953f6ebd027b77330a1c2b9f0a5847501ffffffff020000000000000000007096050d000000002321027f2101a1eb0ef4f53ab48919a0f390dfcaddd6b8104324b4ef8d2654b08b3722ac00000000"
+
+    # decoded by ppcoind
+    decoded_raw_tx = {
+        'vout': [
+            {'scriptPubKey': '', 'value': 0.0},
+            {'scriptPubKey': '21027f2101a1eb0ef4f53ab48919a0f390dfcaddd6b8104324b4ef8d2654b08b3722ac',
+                'value': 218.47}
+        ],
+        'timestamp': 1479378005,
+        'version': 1,
+        'vin': [
+            {
+                'vout': 2,
+                'sequence': 4294967295,
+                'scriptSig': '493046022100b4de95eb9652b86fe3070ab9c1ccf60ec700638848589416f48bfaa0342c2d46022100ed7ff3ec83324c146e3ce8123854a62a9953f6ebd027b77330a1c2b9f0a5847501',
+                'txid': 'f5bb2499be92fd169fae51ce73e258cec2ae1b96c339ef9e79d27ec09c123e89'
+            }
+        ],
+        'locktime': 0
+    }
+
+    assert unpack_raw_transaction(rawtx=rawtx, network="ppc") == decoded_raw_tx
+
+"""
     def test_transaction_assembly_test(self):
         '''verifies that transaction assembly is functioning properly'''
 
@@ -63,6 +129,4 @@ class TransactionsTestCase(unittest.TestCase):
 
         # Match the remainder
         self.assertEqual(hexlify(split[1]), b'020000006b483045022057a5995013c8c55a16c1f692d91881fef443a467316d73a15abd65b6ca6c77dd022100f349283acebe70c2be16dcfd7860aa530e920e74f7a4afeb905d58d73e381ce2012103cd1236a7327457047f596e621b7dfa4a923cfdafffd6094e12db09f0f5695b4dffffffff01c0d45407000000001976a9141e667ee94ea8e62c63fe59a0269bb3c091c86ca388ac00000000')
-
-if __name__ == '__main__':
-    unittest.main()
+"""
