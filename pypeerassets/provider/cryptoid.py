120f274f624239d73482303d46af943f28283d94
Peerchemist <dx.1178@gmail.com>
2017-07-28 20:43:58 +0200

    initial support for cryptoid provider.

---
+++ pypeerassets/provider/cryptoid.py
@@ -0,0 +1,54 @@
+import requests
+
+
+class Cryptoid:
+
+    '''API wrapper for mintr.org blockexplorer,
+    it only implements queries relevant to peerassets.
+    This wrapper does some tweaks to output to match original RPC response.'''
+
+    @classmethod
+    def __init__(self, network: str):
+        """
+        : network = ppc, tppc ...
+        """
+
+        self.net = network
+        self.api_session = requests.Session()
+
+    key = '7547f94398e3'
+    private = ('getbalance', 'unspent')
+
+    @classmethod
+    def req(self, query: str):
+
+        addr = 'http://chainz.cryptoid.info/{0}/api.dws'.format(self.net)
+        query = addr + "?q=" + query
+        if (p in self.private for p in query):
+            query += "&key=" + self.key
+        response = self.api_session.get(query)
+
+        assert response.status_code == 200, {'error': 'API error: ' + str(response.status_code)}
+        return response.json()
+
+    @classmethod
+    def getblockcount(cls) -> int:
+
+        return cls.req('getblockcount')
+
+    @classmethod
+    def getdifficulty(cls) -> float:
+
+        return cls.req('getdifficulty')
+
+    def getbalance(cls, address: str) -> float:
+
+        return float(cls.req('getbalance' + "&a=" + address))
+
+    def getreceivedbyaddress(cls, address: str) -> float:
+
+        return float(cls.req('getreceivedbyaddress' + "&a=" + address))
+
+    def listunspent(cls, address: str) -> list:
+
+        return cls.req('unspent' + "&a=" + address)['unspent_outputs']
