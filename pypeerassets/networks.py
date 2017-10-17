bda252fe1ec1cc7e9fb45e6b44c6208fefb18a59
Peerchemist <dx.1178@gmail.com>
2017-10-17 12:41:00 +0200

    add min_vout_value to networks

--- pypeerassets/networks.py
+++ pypeerassets/networks.py
@@ -9,6 +9,7 @@ Network = namedtuple('Network', [
     'magicbytes',
     'msgPrefix',
     'min_tx_fee',
+    'min_vout_value',
     'tx_timestamp',
     'denomination',
 ])
@@ -22,16 +23,16 @@ For abbreviation prefix testnet of the network with "t".
 networks = (
     # Peercoin mainnet
     Network("peercoin", "ppc", b'37', b'b7', b'75', b'e6e8e9e5',
-            b'\x17PPCoin Signed Message:\n', float(0.01), True, 1000000),
+            b'\x17PPCoin Signed Message:\n', float(0.01), float(0.01), True, 1000000),
     # Peercoin testnet
     Network("peercoin-testnet", "tppc", b'6f', b'ef', b'c4', b'cbf2c0ef',
-            b'\x17PPCoin Signed Message:\n', float(0.01), True, 1000000),
+            b'\x17PPCoin Signed Message:\n', float(0.01), float(0.01), True, 1000000),
     # Bitcoin mainnet
     Network("bitcoin", "btc", b'00', b'80', b'05', b'd9b4bef9',
-            b'\x18Bitcoin Signed Message:\n', 0, False, 100000000),
+            b'\x18Bitcoin Signed Message:\n', 0, 0, False, 100000000),
     # Bitcoin testnet
     Network("bitcoin-testnet", "tbtc", b'6f', b'ef', b'c4', b'dab5bffa',
-            b'\x18Bitcoin Signed Message:\n', 0, False, 100000000)
+            b'\x18Bitcoin Signed Message:\n', 0, 0, False, 100000000)
 )
 
 
