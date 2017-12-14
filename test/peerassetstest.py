ca758abbd091fecd51fea2f5af0288d03cfb1bce
Peerchemist <dx.1178@gmail.com>
2017-12-14 12:36:04 +0100

    add tests for finding deck and cards.

--- test/peerassetstest.py
+++ test/peerassetstest.py
@@ -1,85 +1,35 @@
-import unittest
-from pypeerassets.main import parse_deckspawn_metainfo
-from pypeerassets import Kutil
-from pypeerassets.main import Deck
+import pytest
+import pypeerassets as pa
 
-"""
-class PeerAssetsTestCase(unittest.TestCase):
-    '''tests for protobuf manipulation'''
 
-    @classmethod
-    def setUpClass(cls):
-        print('''Starting PeerAssets tests.
-                This class handles all things PeerAssets protocol.''')
+def test_find_deck():
 
-    def test_parse_deckspawn_metainfo(self):
-        '''tests if loading of deck parameteres from protobuf'''
+    provider = pa.Cryptoid(network="tppc")
 
-        string = b'\x08\x01\x12\x0cmy_test_deck\x18\x03 \x02'
-        self.assertEqual(parse_deckspawn_metainfo(string), {'issue_mode': 2,
-                                                            'name': 'my_test_deck',
-                                                            'number_of_decimals': 3,
-                                                            'version': 1})
+    deck = pa.find_deck(provider, 'b6a95f94fef093ee9009b04a09ecb9cb5cba20ab6f13fe0926aeb27b8671df43', 1)
 
-    def test_parse_invalid_deckspawn_metainfo(self):
-        '''test if it fails on faulty deckspawn metainfo'''
+    assert deck.__dict__ == {'asset_specific_data': b'',
+                             'fee': 0.0,
+                             'id': 'b6a95f94fef093ee9009b04a09ecb9cb5cba20ab6f13fe0926aeb27b8671df43',
+                             'issue_mode': 4,
+                             'issue_time': 1488840533,
+                             'issuer': 'msYThv5bf7KjhHT1Cj5D7Y1tofyhq9vhWM',
+                             'name': 'hopium_v2',
+                             'network': 'peercoin-testnet',
+                             'number_of_decimals': 2,
+                             'production': True,
+                             'testnet': True,
+                             'version': 1
+                             }
 
-        string = b'\x12\x06faulty\x18\x01' # without version and issue mode
-        self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
 
-        string = b'\x08\x01\x18\x05 \x04' # without deck name
-        self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
+def test_find_cards():
 
-        string = b'\x08\x01\x12\x06faulty \x02' # without number_of_decimals
-        self.assertRaises(AssertionError, parse_deckspawn_metainfo, string)
-"""
+    provider = pa.Cryptoid(network="tppc")
 
-class P2THTest(unittest.TestCase):
+    deck = pa.find_deck(provider, 'b6a95f94fef093ee9009b04a09ecb9cb5cba20ab6f13fe0926aeb27b8671df43', 1)
 
-    @classmethod
-    def setUpClass(cls):
-        print('''Starting P2TH test...''')
+    cards = list(pa.find_card_transfers(provider, deck))
 
-        cls.txid = "c23375caa1ba3b0eec3a49fff5e008dede0c2761bb31fddd830da32671c17f84"
-
-    def test_p2th_address_generation(self):
-        '''generating deck p2th from deck spawn id'''
-
-        key = Kutil(network="ppc", privkey=self.txid)
-
-        self.assertEqual(key.address, "PRoUKDUhA1vgBseJCaGMd9AYXdQcyEjxu9")
-        self.assertEqual(key.wif, "UBctiEkfxpU2HkyTbRKjiGHT5socJJwCny6ePfUtzo8Jad9wVzeA")
-
-    def test_testnet_p2th_address_generation(self):
-        '''generating testnet deck p2th from deck spawn id'''
-
-        key = Kutil(network="tppc", privkey=self.txid.encode())
-
-        self.assertEqual(key.address, "mxjFTJApv7sjz9T9a4vCnAQbmsqSoL8VWo")
-        self.assertEqual(key.wif, "cU6CjGw3mRmirjiUZfRkJ1aj2D493k7uuhywj6tCVbLAMABy4MwU")
-
-class DeckInitTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        print('''Starting Deck initialization...''')
-
-    def test_deck_to_dict(self):
-        '''test deck to dict'''
-
-        my_deck = Deck(version=1, name="Tabula Rasa", number_of_decimals=1, issue_mode="MULTI")
-        self.assertEqual(my_deck.metainfo_to_dict,
-                         {'issue_mode': 'MULTI',
-                          'name': 'Tabula Rasa',
-                          'number_of_decimals': 1,
-                          'version': 1}
-                        )
-
-    def test_deck_to_protobuf(self):
-        '''test deck to protobuf'''
-
-        my_deck = Deck(version=1, name="Tabula Rasa", number_of_decimals=1, issue_mode="MULTI")
-        self.assertEqual(my_deck.metainfo_to_protobuf, b'\x08\x01\x12\x0bTabula Rasa\x18\x01 \x04')
-
-if __name__ == '__main__':
-    unittest.main()
+    assert cards
+    assert isinstance(cards[0], pa.CardTransfer)
