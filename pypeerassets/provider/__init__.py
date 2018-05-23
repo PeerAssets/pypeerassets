decd3444be02797520b2f9525b309b60017cbb38
Peerchemist <dx.1178@gmail.com>
2018-05-23 17:56:45 +0200

    dropping Holy

--- pypeerassets/provider/__init__.py
+++ pypeerassets/provider/__init__.py
@@ -1,6 +1,5 @@
 from .rpcnode import RpcNode
 from .common import Provider
 from .mintr import Mintr
-from .holytransaction import Holy
 from .cryptoid import Cryptoid
 from .explorer import Explorer
