cf8322c3e0256cbbb95409ba8163a0df00906655
Peerchemist <dx.1178@gmail.com>
2018-05-25 22:49:44 +0200

    placeholders for future tests

--- test/test_transactions.py
+++ test/test_transactions.py
@@ -8,3 +8,12 @@ from pypeerassets.transactions import *
 def test_calculate_transaction_fee(tx_size):
 
     assert round(calculate_tx_fee(tx_size), 2) == round(Decimal(0.01), 2)
+
+
+def test_nulldata_script():
+    pass
+
+
+def test_p2pkh_script():
+    pass
+
