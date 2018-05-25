cfb2ea773ecf52a97a1d29db64fa166eff52aa3b
Peerchemist <dx.1178@gmail.com>
2018-05-25 18:23:37 +0200

    implement calculate_transaction_fee test

--- test/test_transactions.py
+++ test/test_transactions.py
@@ -1,4 +1,10 @@
 import pytest
-from binascii import unhexlify, hexlify
+from decimal import Decimal, getcontext
+getcontext().prec = 6
 from pypeerassets.transactions import *
 
+
+@pytest.mark.parametrize("tx_size", [181, 311])
+def test_calculate_transaction_fee(tx_size):
+
+    assert round(calculate_tx_fee(tx_size), 2) == round(Decimal(0.01), 2)
