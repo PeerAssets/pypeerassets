ae7cc245938b1e02974f9b54830146019ca9c0c1
Peerchemist <dx.1178@gmail.com>
2018-05-30 14:25:33 +0200

    make imports init __init__ prettier

--- pypeerassets/__init__.py
+++ pypeerassets/__init__.py
@@ -1,3 +1,10 @@
 from pypeerassets.kutil import Kutil
 from pypeerassets.provider import *
-from pypeerassets.__main__ import *
+from pypeerassets.__main__ import (deck_parser,
+                                   find_all_valid_cards,
+                                   find_all_valid_decks,
+                                   find_deck,
+                                   deck_spawn,
+                                   deck_transfer,
+                                   get_card_transfers,
+                                   card_transfer)
