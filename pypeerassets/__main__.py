f01a180f8cca1c7ceb05158dc139f06e371abcc0
saeveritt <saeveritt@gmail.com>
2018-06-06 16:05:36 -0500

    Try Except TypeError from e ( validate card p2th )

--- pypeerassets/__main__.py
+++ pypeerassets/__main__.py
@@ -198,7 +198,7 @@ def get_card_transfers(provider: Provider, deck: Deck) -> Generator:
 
         except (InvalidCardTransferP2TH, CardVersionMismatch,
                 CardNumberOfDecimalsMismatch, InvalidVoutOrder,
-                RecieverAmountMismatch, DecodeError) as e:
+                RecieverAmountMismatch, DecodeError, TypeError) as e:
             return []
 
         return cards
--- pypeerassets/pautils.py
+++ pypeerassets/pautils.py
@@ -199,12 +199,11 @@ def validate_card_transfer_p2th(deck: Deck, raw_tx: dict) -> None:
     
     try:
         address = raw_tx["vout"][0]["scriptPubKey"].get("addresses")[0]
+        if not address == deck.p2th_address:
+            raise InvalidCardTransferP2TH(error)
     except TypeError as e:
         raise Exception(error) from e
-
-    if not address == deck.p2th_address:
-        raise InvalidCardTransferP2TH(error)
-
+f
 
 def parse_card_transfer_metainfo(protobuf: bytes, deck_version: int) -> dict:
     '''decode card_spawn protobuf message and validate it against deck.version
