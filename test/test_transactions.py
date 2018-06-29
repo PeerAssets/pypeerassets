66ca4b9368ec6aaee9daf2de791a86df608aa575
Peerchemist <dx.1178@gmail.com>
2018-06-29 21:58:18 +0200

    improve transactions test

--- test/test_transactions.py
+++ test/test_transactions.py
@@ -1,25 +1,44 @@
-from decimal import Decimal, getcontext
+
+
+import pytest
+from decimal import Decimal
 
 from btcpy.structs.transaction import (
     Locktime,
+    TxOut,
     PeercoinTx,
-    Transaction,
+    Transaction
 )
-import pytest
 
 from pypeerassets.transactions import (
     calculate_tx_fee,
     make_raw_transaction,
+    p2pkh_script,
+    tx_output
 )
-
-
-getcontext().prec = 6
 
 
 @pytest.mark.parametrize("tx_size", [181, 311])
 def test_calculate_transaction_fee(tx_size):
 
     assert round(calculate_tx_fee(tx_size), 2) == round(Decimal(0.01), 2)
+
+
+@pytest.mark.parametrize("network", ['bitcoin', 'peercoin'])
+def test_tx_output(network):
+
+    if network == 'peercoin':
+        addr = 'PAdonateFczhZuKLkKHozrcyMJW7Y6TKvw'
+    if network == 'bitcoin':
+        addr = '1FV9w4NvBnnNp4GMUNuqfzqGKvgBY5YTSB'
+
+    script = p2pkh_script(network, addr)
+
+    txout = tx_output(network=network, value=Decimal(0),
+                      n=1, script=script
+                      )
+
+    assert isinstance(txout, TxOut)
 
 
 def test_nulldata_script():
