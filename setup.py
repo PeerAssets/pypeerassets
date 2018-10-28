274cdb3f4c061bd608a7b1db6f2b599e0b0a8a10
Peerchemist <dx.1178@gmail.com>
2018-10-28 13:59:54 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypeerassets',
-      version='0.4.7',
+      version='0.4.8',
       description='Python implementation of the PeerAssets protocol.',
       keywords=["blockchain", "digital assets", "protocol"],
       url='https://github.com/peerassets/pypeerassets',
