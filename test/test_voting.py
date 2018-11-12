f63b7ad136ea1d7a8a4197c8ea11650767d1fb90
Peerchemist <dx.1178@gmail.com>
2018-11-12 11:14:59 +0100

    more work on tests

--- test/test_voting.py
+++ test/test_voting.py
@@ -26,11 +26,11 @@ deck = Deck(
 vote = voting.VoteInit(
             deck=deck,
             version=1,
-            start_block=1,
-            end_block=100,
+            start_block=350000,
+            end_block=400000,
             count_mode=1,  # SIMPLE vote count method
-            choices=("11",
-                      "3"),
+            choices=["11",
+                      "3"],
             description="",
             id="0fce7f493038abb8aaa8f5b3e8130d01e5804c8dee9a19202c6cceae7c8e5e27",
             vote_metainfo="https://imgur.com/my_pic.png",
