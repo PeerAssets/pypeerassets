232e2c3c5cd971f3c8161c338b192fe61faa2e7d
Peerchemist <dx.1178@gmail.com>
2019-05-30 13:09:15 +0200

    version bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypeerassets',
-      version='0.4.7',
+      version='0.4.8',
       description='Python implementation of the PeerAssets protocol.',
       keywords=["blockchain", "digital assets", "protocol"],
       url='https://github.com/peerassets/pypeerassets',
@@ -9,5 +9,5 @@ setup(name='pypeerassets',
       author_email='peerchemist@protonmail.ch',
       license='BSD',
       packages=['pypeerassets', 'pypeerassets.provider'],
-      install_requires=['protobuf', 'peerassets-btcpy', 'peercoin_rpc>=0.5.6']
+      install_requires=['protobuf', 'peerassets-btcpy', 'peercoin_rpc']
       )
